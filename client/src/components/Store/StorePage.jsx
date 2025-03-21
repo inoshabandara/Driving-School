3b717219337fd64457f3a37151316fed9ceadd4a
inoshabandara <inoshabandara55155@gmail.com>
2025-03-21 11:06:42 +0530

    Create StorePage.jsx

---
+++ client/src/components/Store/StorePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Store from './Store';
+import PackagesPage from './PackagesPage';
+
+const StorePage = () => {
+  return (
+    <div>
+      <Store />
+      <PackagesPage />
+    </div>
+  );
+};
+
+export default StorePage;
